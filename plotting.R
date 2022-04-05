68c15ca13e528dfff1ff5f8cda8245dc1c94aadf
lillian.nguyen@portlandoregon.gov <lillian.nguyen@portlandoregon.gov>
2022-04-05 09:51:33 -0700

    Clean FRED plot x-axis date labels

--- plotting.R
+++ plotting.R
@@ -4,7 +4,10 @@ plot_fred <- function(df) {
       ggplot(aes(x = date, y = value, group = series_id)) +
       geom_line(aes(color = series_id)) +
       scale_y_continuous() +
+      scale_x_date(date_breaks = "3 months" , date_labels = "%b %Y") +
       scale_color_viridis(discrete=TRUE) +
-      theme_minimal()
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 45),
+            axis.title.x = element_blank())
   )
 }
