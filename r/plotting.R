30e356cb6e20f3c3e6ec7a7912e6f1515c3a1288
lillian.nguyen@portlandoregon.gov <lillian.nguyen@portlandoregon.gov>
2022-05-12 09:29:13 -0700

    Generalize the plotting function

--- r/plotting.R
+++ r/plotting.R
@@ -1,13 +1,22 @@
-plot_fred <- function(df) {
-  ggplotly(
-    df %>%
-      ggplot(aes(x = date, y = value, group = series_id)) +
-      geom_line(aes(color = series_id)) +
-      scale_y_continuous() +
+make_plot <- function(df, x = date, y = value, group = NULL) {
+  
+  if (is.null(group)) {
+    plot <- df %>%
+      ggplot(aes(x = date, y = value)) +
+      geom_line()
+  } else {
+    plot <- df %>%
+      ggplot(aes_string(x = "date", y = "value", group = group)) +
+      geom_line(aes_string(color = group))
+  }
+
+  plot <- plot +
+      scale_y_continuous(labels = comma) +
       scale_x_date(date_breaks = "3 months" , date_labels = "%b %Y") +
       scale_color_viridis(discrete=TRUE) +
       theme_minimal() +
-      theme(axis.text.x = element_text(angle = 45),
+      theme(axis.text.x = element_text(angle = 90),
             axis.title.x = element_blank())
-  )
+  
+  ggplotly(plot)
 }
