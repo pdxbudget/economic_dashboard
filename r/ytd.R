d7b106a766ea3ba5d6ae9d21df1de174b1e11ed3
lillian.nguyen@portlandoregon.gov <lillian.nguyen@portlandoregon.gov>
2022-07-05 10:08:46 -0700

    Generalize ytd function to pull current or last calendar year

--- r/ytd.R
+++ r/ytd.R
@@ -1,7 +1,20 @@
-get_ytd_current <- function(df, date, value, params) {
-  df %>%
-  filter(!!sym(date) < (params$end_date + months(1)),
-         !!sym(date) > (floor_date(params$end_date, unit = "year") - months(1))) %>%
+get_ytd <- function(df, type, date, value, params) {
+  
+  # date: string, colname with date values
+  # value: string, colname with values
+  
+  if (type == "current") {
+    df <- df %>%
+      # adding 1 month to avoid <= and >=, which was causing issues with the dynamic colnames
+      filter(!!sym(date) < (params$end_date + months(1)),
+             !!sym(date) > (floor_date(params$end_date, unit = "year") - months(1)))
+  } else if (type == "last") {
+    df <- df %>%
+      filter(!!sym(date) < (params$end_date - years(1)) + months(1),
+             !!sym(date) > (floor_date(params$end_date, unit = "year") - years(1)) + months(1))
+  }
+  
+ df %>%
   summarize(!!value := sum(!!sym(value), na.rm = TRUE)) %>%
   pull(value)
 }
